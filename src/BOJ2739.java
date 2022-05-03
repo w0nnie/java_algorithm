d94f362156460a9d59c4103fa34960d6848fdb4a
jinwon <kjw55523@gmail.com>
2022-05-03 15:28:33 +0900

    BOJ2739

--- src/BOJ2739.java
+++ src/BOJ2739.java
@@ -4,7 +4,7 @@ public class BOJ2739 {
     public static void main(String args[]) {
         Scanner scanner= new Scanner(System.in);
         int a = scanner.nextInt();
-       for (int i=0; i<=9; i++){
+       for (int i=1; i<=9; i++){
            System.out.println(a + " * " + i + " = " + a*i);
        }
         scanner.close();
