e9ef67541e208e75a441969ac05c888764708003
jinwon <kjw55523@gmail.com>
2022-05-07 18:58:46 +0900

    BOJ2557

--- src/BOJ2557.java
+++ src/BOJ2557.java
@@ -1,8 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class BOJ2557 {
 
-    public static void main(String args[]){
+    public static void main(String args[]) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.print("Hello World!");
+
+
+            br.close();
     }
 }
+
 
